40e081354e405596fd7af53c69804f7856ea2243
GaryGosh <clichethe1@yahoo.com>
2020-08-14 06:09:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+import { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+    }
+
+    console.log('token bruh', _token);
+  }, []);
+
+  console.log('user', user);
+
+  return (
+    <div className="app">
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
+            
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div>
+            <h1>Welcome bruh</h1>
+        </div>
+    )
+}
+
+export default Player
